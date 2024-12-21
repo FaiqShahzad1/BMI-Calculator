051bb03d38dad66da834a4322edebdc3e435dba4
Faiqshahzad1 <laibanice25@gmail.com>
2024-12-21 17:41:10 +0500

    Feat: started making BMI Calculator

---
+++ script.mjs
@@ -0,0 +1,10 @@
+function CalculateBMI() {
+let heightinputfeet = document.getElementById("heightinputfeet").value;
+let heightinputinch = document.querySelector("#heightinputinch").valet
+
+let weightinput = document.querySelector("#weightinput").value;
+
+heightinputfeet = parseInt(weightinput);
+heightinputinch = parseInt(weightinput);
+
+}
